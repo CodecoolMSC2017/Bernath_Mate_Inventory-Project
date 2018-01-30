79bd0e5f53c305c5aed5f10c1c61bfea110820d7
Bernáth Máté <bernath.mate.new@gmail.com>
2018-01-30 14:53:10 +0100

    Structure of classes is implemented

---
+++ src/main/java/com/codecool/StoreManager.java
@@ -0,0 +1,24 @@
+package com.codecool;
+
+public class StoreManager {
+
+    public String addToStorage(StorageCapable storage) {
+        return "";
+    }
+
+    public void addCDProduct(String name, int price, int tracks) {
+
+    }
+
+    public void addBookProduct(String name, int price, int pages) {
+
+    }
+
+    public String listProducts() {
+        return "";
+    }
+
+    public int getTotalProductPrice() {
+        return 0;
+    }
+}
