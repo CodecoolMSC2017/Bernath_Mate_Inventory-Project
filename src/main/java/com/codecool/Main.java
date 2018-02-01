d07fd55d6572f26ee6d5961ab2723e0e00c6d5b3
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-01 18:51:49 +0100

    Main class is implemented

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,6 +3,17 @@ package com.codecool;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        PersistentStore ps = new PersistentStore();
+        StoreManager manager = new StoreManager();
+        manager.addStorage(ps);
+        manager.addCDProduct("Master of puppets", 1500, 10);
+        manager.addBookProduct("Bölcsek Köve", 2500, 500);
+        ps.store("Products.xml");
+        ps.loadProducts("Products.xml");
+        System.out.println(manager.listProducts());
+        System.out.println(manager.getTotalProductPrice());
+
     }
+
 }
