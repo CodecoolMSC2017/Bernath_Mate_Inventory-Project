c2300ba4524401e35b1112be3ae98c8df2e0de96
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-13 12:14:13 +0100

    main method is modified to store products to csv file.

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -5,10 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         PersistentStore ps = new PersistentStore();
+        PersistentStoreToCsv psCsv = new PersistentStoreToCsv();
         StoreManager manager = new StoreManager();
+        manager.addStorage(psCsv);
         manager.addStorage(ps);
-        manager.addCDProduct("Master of puppets", 1500, 10);
+        manager.addCDProduct("Metallica: Master of puppets", 1500, 10);
+        manager.addCDProduct("Akkezdet Phiai: Akkezdet", 2500, 12);
         manager.addBookProduct("Bölcsek Köve", 2500, 500);
+        manager.addBookProduct("Slam.pont!", 2200, 60);
+        psCsv.store("Products.csv");
         ps.store("Products.xml");
         ps.loadProducts("Products.xml");
         System.out.println(manager.listProducts());
