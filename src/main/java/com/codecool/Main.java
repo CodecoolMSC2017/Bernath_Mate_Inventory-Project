0df21fac3e9229408cff8ff2558e3f8fb3af9a90
Bernáth Máté <bernath.mate.new@gmail.com>
2018-02-13 13:54:44 +0100

    main method is modified, xml and csv StoreManagers are separated

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -4,20 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // xml
         PersistentStore ps = new PersistentStore();
-        PersistentStoreToCsv psCsv = new PersistentStoreToCsv();
         StoreManager manager = new StoreManager();
-        manager.addStorage(psCsv);
         manager.addStorage(ps);
         manager.addCDProduct("Metallica: Master of puppets", 1500, 10);
-        manager.addCDProduct("Akkezdet Phiai: Akkezdet", 2500, 12);
         manager.addBookProduct("Bölcsek Köve", 2500, 500);
-        manager.addBookProduct("Slam.pont!", 2200, 60);
-        psCsv.store("Products.csv");
         ps.store("Products.xml");
-        ps.loadProducts("Products.xml");
-        System.out.println(manager.listProducts());
-        System.out.println(manager.getTotalProductPrice());
+        System.out.println("Products from xml: " +  manager.listProducts());
+        System.out.println("Total price: " + manager.getTotalProductPrice());
+
+        // csv
+        PersistentStoreToCsv psCsv = new PersistentStoreToCsv();
+        StoreManager csvManager = new StoreManager();
+        csvManager.addStorage(psCsv);
+        csvManager.addCDProduct("Akkezdet Phiai: Akkezdet", 2500, 12);
+        csvManager.addBookProduct("Slam.pont!", 2200, 60);
+        psCsv.store("Products.csv");
+        System.out.println("Products from csv: " +  csvManager.listProducts());
+        System.out.println("Total price: " + csvManager.getTotalProductPrice());
 
     }
 
